3387f23aa903fc4f6271badddbe8640dd065e19d
NemoBJJ <engnfaraujo@gmail.com>
2024-12-21 12:28:42 -0300

    ApiRestFull Financeiro

--- src/main/java/com/nemuel/financeiro/api/entity/Transaction.java
+++ src/main/java/com/nemuel/financeiro/api/entity/Transaction.java
@@ -1,20 +1,33 @@
 package com.nemuel.financeiro.api.entity;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "transactions") // Define o nome correto da tabela
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
+
     private Double amount;
+
     private LocalDate date;
+
+    private String type;
+
+    @Column(name = "transaction_category") // Nome da coluna no banco
+    private String transaction_category;
+
+    @Column(name = "transaction_month") // Nome da coluna no banco
+    private String transaction_month;
 
     // Getters e Setters
     public Long getId() {
@@ -47,5 +60,29 @@ public class Transaction {
 
     public void setDate(LocalDate date) {
         this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTransaction_category() {
+        return transaction_category;
+    }
+
+    public void setTransaction_category(String transaction_category) {
+        this.transaction_category = transaction_category;
+    }
+
+    public String getTransaction_month() {
+        return transaction_month;
+    }
+
+    public void setTransaction_month(String transaction_month) {
+        this.transaction_month = transaction_month;
     }
 }
